0f4d4ff2176be76057812dfe2493432639e76c39
Santiago Higuera <shiguera@mercatorlab.com>
2015-02-26 16:43:30 +0100

    Initial commit

---
+++ src/main/java/com/mlab/vlc/App.java
@@ -0,0 +1,13 @@
+package com.mlab.vlc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
